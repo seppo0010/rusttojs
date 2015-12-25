0d7769dbf04adf29de7ef15bcc3d743579636346
Sebastian Waisbrot <swaisbrot@twitter.com>
2015-12-25 09:40:13 -0800

    Basher test runner

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-./rusttojs test/arguments.rs|diff - test/arguments.js
-./rusttojs test/helloworld.rs|diff - test/helloworld.js
-./rusttojs test/let.rs|diff - test/let.js
-./rusttojs test/main.rs|diff - test/main.js
-./rusttojs test/print-number.rs|diff - test/print-number.js
+for FILE in $(ls test/*.js); do
+  ./rusttojs ${FILE: 0: ${#FILE}-3}.rs|diff - ${FILE}
+done
